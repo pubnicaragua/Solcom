26775a5834bf17e225696623a7fe16c83d8fe68d
pubnicaragua <clientesactivos4@gmail.com>
2026-01-27 18:31:12 -0600

    fix: Agregar vercel.json y configurar output standalone para Vercel

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   images: {
     domains: ['www.soliscomercialni.com'],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
   },
 }
 
